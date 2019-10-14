33a3bec92c446342e872e4e8f85ca539843b2946
songam <songam@jfinfo.com>
2019-10-14 18:20:27 +0800

    im ClueSrv.DoPush

--- src/test/java/clue/services/push/PusherTest.java
+++ src/test/java/clue/services/push/PusherTest.java
@@ -37,37 +37,6 @@ public class PusherTest {
     OrgSrv orgSrv;
 
 
-
-    @Test
-    public void a(){
-
-
-//        ClUtil clUtil = new ClUtil();
-//        clUtil.setIdstr("pushing_max_clid");
-//        clUtil.setValue("-1"); //初始值是-1，之后为未处理的起始cl_id
-//        clUtil.setRemart("正在推送的最clid值，以协助多线程乐观锁推送");
-//        clUtil.setCreatetime(DateTime.now().getMillis());
-//        clUtil.setLasttime(DateTime.now().getMillis());
-//        clUtilDao.insert(clUtil);
-
-//        ClUtil clUtil = clUtilDao.selectByPrimaryKey((long)1);
-//        ClUtilExample clUtilExample = new ClUtilExample();
-//        ClUtilExample.Criteria c = clUtilExample.createCriteria();
-//        c.andIdstrEqualTo(clUtil.getIdstr());
-//        c.andValueEqualTo(clUtil.getValue());
-//        clUtil.setValue(new Long(500).toString());
-//        int n = clUtilDao.updateByExampleSelective(clUtil,clUtilExample);
-//        assert n>0;
-
-        List<ClClue> list = clClueDao.selectNotPush(44,DateTime.now().plusDays(-3).getMillis(),200);
-        assert list.size()>0;
-
-    }
-
-
-    @Resource
-    PreClCues pcs;
-
     @Test
     public void push(){
         //创建机构
